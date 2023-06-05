101adc404262cca3335e67293ae45d002ee52349
Jose Marcelo Simao <josesimao@guarulhos.sp.gov.br>
2023-06-05 14:45:56 -0300

    Ajustes para execução no Apache Tomcat!

--- src/main/java/br/com/josemarcelo/CafeteriaOut/CafeteriaApplication.java
+++ src/main/java/br/com/josemarcelo/CafeteriaOut/CafeteriaApplication.java
@@ -2,9 +2,10 @@ package br.com.josemarcelo.CafeteriaOut;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CafeteriaApplication {
+public class CafeteriaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CafeteriaApplication.class, args);
